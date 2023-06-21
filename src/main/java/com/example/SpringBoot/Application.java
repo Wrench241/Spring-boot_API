3148fd6ebe8828b91546201f9f722660348dd047
wrench241 <nickalex241@gmail.com>
2023-06-21 16:09:40 -0300

    fix bug create tables

--- src/main/java/com/example/SpringBoot/Application.java
+++ src/main/java/com/example/SpringBoot/Application.java
@@ -1,9 +1,14 @@
 package com.example.SpringBoot;
 
+import com.example.SpringBoot.models.ProductModel;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackageClasses= {ProductModel.class})
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/SpringBoot/models/ProductModel.java
+++ src/main/java/com/example/SpringBoot/models/ProductModel.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 import java.util.UUID;
 
 @Entity
-@Table(name = "tb_products")
+@Table(name = "TB_PRODUCTS")
 public class ProductModel implements Serializable {
     private static final long serialVersionUID = 1L;
 
